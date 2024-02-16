121fb6aad6c9f09a114c22041ccc9b3a0e5e3bb5
Rizki Darmawan <rizdar.contact@gmail.com>
2024-02-17 06:23:13 +0700

    configuring database

--- src/application/database.js
+++ src/application/database.js
@@ -1,39 +1,41 @@
-import {PrismaClient} from "@prisma/client";
-import {logger} from "./logging.js";
+import { PrismaClient } from '@prisma/client';
+import { logger } from './logging.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const prismaClient = new PrismaClient({
-    log: [
-        {
-            emit: 'event',
-            level: 'query',
-        },
-        {
-            emit: 'event',
-            level: 'error',
-        },
-        {
-            emit: 'event',
-            level: 'info',
-        },
-        {
-            emit: 'event',
-            level: 'warn',
-        },
-    ],
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'event',
+      level: 'error',
+    },
+    {
+      emit: 'event',
+      level: 'info',
+    },
+    {
+      emit: 'event',
+      level: 'warn',
+    },
+  ],
 });
 
 prismaClient.$on('error', (e) => {
-    logger.error(e);
+  logger.error(e);
 });
 
 prismaClient.$on('warn', (e) => {
-    logger.warn(e);
+  logger.warn(e);
 });
 
 prismaClient.$on('info', (e) => {
-    logger.info(e);
+  logger.info(e);
 });
 
 prismaClient.$on('query', (e) => {
-    logger.info(e);
+  logger.info(e);
 });
